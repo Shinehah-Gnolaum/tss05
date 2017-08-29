6a7bb2ae43887bd17a8938e7dffc93d8be8bce91
Shinehah-Gnolaum <shinehah.gnolaum@icloud.com>
2017-08-28 22:15:07 -0500

    added ContactMailable and contact.blade.php

---
+++ app/Mail/ContactMailable.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Http\Request;
+
+class ContactMailable extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $request;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.contact')
+        ->to('dbrower256@cableone.net')
+        ->subject('message from texarkanasoftware.com website - ' . $this->request->subject);
+    }
+}
