3d003e7267b8f27f86b430ed37e3b6eee57ba7b5
Shinehah-Gnolaum <shinehah.gnolaum@icloud.com>
2017-09-01 00:32:30 -0500

    added 'texarkana' to meta tag keywords

--- resources/views/layouts/meta.blade.php
+++ resources/views/layouts/meta.blade.php
@@ -1,6 +1,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
   	<meta name="description" content="Web Designer and Software Developer">
-  	<meta name="keywords" content="web desing, programmer, web designer, software design">
+  	<meta name="keywords" content="web design, programmer, web designer, software design, web design texarkana, programmer texarkana, web designer texarkana, software design texarkana">
   	<meta name="author" content="Daniel Brower">
   	<meta name="viewport" content="width=device-width, initial-scale=1.0">
