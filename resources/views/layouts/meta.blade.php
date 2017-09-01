d2676eba02704160ea4d0907ce476d68e8818b77
Shinehah-Gnolaum <shinehah.gnolaum@icloud.com>
2017-09-01 00:14:50 -0500

    added addtitional meta tags for better seo

---
+++ resources/views/layouts/meta.blade.php
@@ -0,0 +1,6 @@
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  	<meta name="description" content="Web Designer and Software Developer">
+  	<meta name="keywords" content="web desing, programmer, web designer, software design">
+  	<meta name="author" content="Daniel Brower">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
